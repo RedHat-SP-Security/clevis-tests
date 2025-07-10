58ee443395f0341a28c6810f6ebe60e05a7f058f
Adam Prikryl <adam.prikryl7@icloud.com>
2025-07-10 17:43:16 +0200

    Fix the test

--- Other/clevis-boot-unlock-all-pins-image-mode/runtest.sh
+++ Other/clevis-boot-unlock-all-pins-image-mode/runtest.sh
@@ -10,15 +10,15 @@
 #
 #   Copyright (c) 2024 Red Hat, Inc.
 #
-#   This program is free software: you can redistribute and/or
+#   This program is free software: you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License as
-#   published by the Free Software Foundation, either version 2 of
-#   the License, or (at your option) any later version.
+#   published by the Free Software Foundation, either version 2 of the
+#   License, or (at your option) any later version.
 #
 #   This program is distributed in the hope that it will be
 #   useful, but WITHOUT ANY WARRANTY; without even the implied
-#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#   PURPOSE.  See the GNU General Public License for more details.
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see http://www.gnu.org/licenses/.
@@ -55,11 +55,11 @@ EOF
 
     # Check for persisted Tang IP
     if [ ! -f "${PERSISTENT_TANG_IP_FILE}" ]; then
-        echo "ERROR: ${PERSISTENT_TANG_IP_FILE} not found!"
-    else
-        echo "Found Tang IP file:"
-        cat "${PERSISTENT_TANG_IP_FILE}"
+        rlDie "ERROR: ${PERSISTENT_TANG_IP_FILE} not found!"
     fi
+
+    rlLogInfo "Found Tang IP file:"
+    cat "${PERSISTENT_TANG_IP_FILE}"
 
     TANG_IP=$(cat "${PERSISTENT_TANG_IP_FILE}" 2>/dev/null)
     rlAssertNotEquals "Tang server IP not found in ${PERSISTENT_TANG_IP_FILE}." "" "$TANG_IP"
@@ -75,41 +75,4 @@ EOF
     else
       rlDie "No TPM2 device found (neither /dev/tpmrm0 nor /dev/tpm0)"
     fi
-    rlRun "ls -l ${TPM_DEVICE}" 0 "Check TPM2 device"
-
-    # Journal checks
-    if rlIsRHELLike '>=10'; then
-      rlRun "journalctl -b | grep 'Finished systemd-cryptsetup'" 0
-    else
-      rlRun "journalctl -b | grep 'Finished Cryptography Setup for luks-'" 0
-      rlRun "journalctl -b | grep 'clevis-luks-askpass.service: Deactivated successfully'" 0
-    fi
-
-    # Root LUKS detection
-    rlRun "lsblk -no FSTYPE,MOUNTPOINT | grep 'crypto_LUKS /'" 0
-    rlRun "sudo cryptsetup status root | grep 'cipher: aes-cbc-essiv:sha256'" 0
-    rlRun "sudo cryptsetup status root | grep 'active one key slot'" 0
-
-    ROOT_LUKS_DEV_CANDIDATE=$(lsblk -o NAME,TYPE,FSTYPE,MOUNTPOINT | awk '$2=="crypt" && $4=="/" {print "/dev/"$1}')
-    if [ -z "$ROOT_LUKS_DEV_CANDIDATE" ]; then
-        ROOT_LUKS_DEV_CANDIDATE=$(lsblk -o NAME,TYPE,MOUNTPOINT,PKNAME | grep ' / ' | awk '{print "/dev/"$4}' | xargs -I {} sh -c 'lsblk -no NAME,TYPE {} | grep crypt | awk "{print \"/dev/\"\$1}"')
-    fi
-    if [ -z "$ROOT_LUKS_DEV_CANDIDATE" ]; then
-        rlLogWarning "Could not auto-detect LUKS device; fallback to /dev/vda2"
-        ROOT_LUKS_DEV_CANDIDATE="/dev/vda2"
-    else
-        rlLogInfo "Detected root LUKS device: ${ROOT_LUKS_DEV_CANDIDATE}"
-    fi
-
-    rlRun "sudo clevis luks list -d ${ROOT_LUKS_DEV_CANDIDATE} | grep 'tang' | grep 'tpm2' | grep 't=2'" 0
-
-    # Tang server test
-    rlLogInfo "Testing reachability of Tang server at ${TANG_IP}:${TANG_SERVER_PORT}..."
-    rlRun "curl -sfg http://${TANG_IP}:${TANG_SERVER_PORT}/adv -o /tmp/adv.jws" 0
-    rlFileExists "/tmp/adv.jws" || rlDie "Tang advertisement not downloaded"
-  rlPhaseEnd
-
-  rlPhaseStartCleanup
-    rlRun "rlFileRestore" 0 "Restore ~/.ssh/"
-  rlPhaseEnd
-rlJournalEnd
+    rlRun "ls -l ${TPM_DEVICE}" 0 "Check TPM2_
