5e9e0e57895aec5886d75919191150adfd97d9d6
Adam Prikryl <adam.prikryl7@icloud.com>
2025-07-24 19:43:57 +0200

    Fix year of the test

--- Other/clevis-boot-unlock-all-pins-multihost/runtest.sh
+++ Other/clevis-boot-unlock-all-pins-multihost/runtest.sh
@@ -8,7 +8,7 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-#   Copyright (c) 2024 Red Hat, Inc.
+#   Copyright (c) 2025 Red Hat, Inc.
 #
 #   This program is free software: you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License as
